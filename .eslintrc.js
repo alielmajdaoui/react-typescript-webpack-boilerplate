bf140e89d7c24ab96b70b4f10cd1cebc7b62c48c
Ali El Majdaoui <alielmajdaouiee@gmail.com>
2022-03-16 21:59:58 +0200

    Change tabs to spaces

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,28 @@
 module.exports = {
-    parser: "@typescript-eslint/parser",
+    parser: '@typescript-eslint/parser',
     extends: [
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react-hooks/recommended",
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
     ],
     parserOptions: {
-        sourceType: "module",
+        sourceType: 'module',
     },
     rules: {
-        "no-mixed-spaces-and-tabs": ["error", "smart-tabs"],
-        "padding-line-between-statements": [
-            "error",
-            { blankLine: "always", prev: "*", next: "return" },
-            { blankLine: "always", prev: ["const", "let", "var"], next: "*" },
+        'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
+        'padding-line-between-statements': [
+            'error',
+            { blankLine: 'always', prev: '*', next: 'return' },
+            { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
             {
-                blankLine: "any",
-                prev: ["const", "let", "var"],
-                next: ["const", "let", "var"],
+                blankLine: 'any',
+                prev: ['const', 'let', 'var'],
+                next: ['const', 'let', 'var'],
             },
-            { blankLine: "always", prev: "directive", next: "*" },
-            { blankLine: "any", prev: "directive", next: "directive" },
-            { blankLine: "always", prev: ["case", "default"], next: "*" },
+            { blankLine: 'always', prev: 'directive', next: '*' },
+            { blankLine: 'any', prev: 'directive', next: 'directive' },
+            { blankLine: 'always', prev: ['case', 'default'], next: '*' },
         ],
-        "no-empty": "warn",
-        "no-else-return": "error",
+        'no-empty': 'warn',
+        'no-else-return': 'error',
     },
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -6,65 +6,65 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const PROJECT_ROOT = __dirname;
 
 module.exports = {
-	mode: 'development',
-	entry: [
-		'webpack-plugin-serve/client',
-		path.join(PROJECT_ROOT, './src/index.tsx'),
-	],
-	output: {
-		path: path.join(PROJECT_ROOT, 'dist'),
-		filename: '[name].js',
-	},
-	target: 'web',
-	resolve: {
-		extensions: ['.js', '.jsx', '.json', '.ts', '.tsx', '.scss', '.css'],
-	},
-	devtool: 'eval',
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: 'src/index.html',
-		}),
-		new ReactRefreshWebpackPlugin(),
-		new Serve({
-			historyFallback: {
-				htmlAcceptHeaders: ['text/html', '*/*'],
-				rewrites: [],
-			},
-			liveReload: false,
-			hmr: true,
-			host: 'localhost',
-			port: 8084,
-			open: false,
-			static: path.join(PROJECT_ROOT, 'dist'),
-		}),
-	],
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: ['babel-loader'],
-			},
-			{
-				test: /\.(ts|tsx)$/,
-				exclude: /node_modules/,
-				use: ['ts-loader'],
-			},
-			{
-				test: /\.(css|scss)$/,
-				use: [
-					process.env.NODE_ENV === 'production'
-						? MiniCssExtractPlugin.loader
-						: 'style-loader',
-					'css-loader',
-					'sass-loader',
-				],
-			},
-			{
-				test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
-				use: ['file-loader'],
-			},
-		],
-	},
-	watch: true,
+    mode: 'development',
+    entry: [
+        'webpack-plugin-serve/client',
+        path.join(PROJECT_ROOT, './src/index.tsx'),
+    ],
+    output: {
+        path: path.join(PROJECT_ROOT, 'dist'),
+        filename: '[name].js',
+    },
+    target: 'web',
+    resolve: {
+        extensions: ['.js', '.jsx', '.json', '.ts', '.tsx', '.scss', '.css'],
+    },
+    devtool: 'eval',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+        }),
+        new ReactRefreshWebpackPlugin(),
+        new Serve({
+            historyFallback: {
+                htmlAcceptHeaders: ['text/html', '*/*'],
+                rewrites: [],
+            },
+            liveReload: false,
+            hmr: true,
+            host: 'localhost',
+            port: 8084,
+            open: false,
+            static: path.join(PROJECT_ROOT, 'dist'),
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: ['ts-loader'],
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                    process.env.NODE_ENV === 'production'
+                        ? MiniCssExtractPlugin.loader
+                        : 'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+                use: ['file-loader'],
+            },
+        ],
+    },
+    watch: true,
 };
